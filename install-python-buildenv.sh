4adb2da6dc302fb0e79233960e40ba98d3ca57cf
Defirence <33593621+Defirence@users.noreply.github.com>
2022-04-25 12:41:51 +0200

    Added ANSI escape codes to show status for INFO etc...
    Signed-off-by: Defirence <33593621+Defirence@users.noreply.github.com>

--- install-python-buildenv.sh
+++ install-python-buildenv.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-echo "[INIT] Installing python-buildenv tools for pyenv..."
-echo "[INFO] Updating apt packages using apt-get update -y"
+echo -e "\e[1;36m[INIT]\e[0m Installing python-buildenv tools for pyenv..."
+echo -e "\e[1;32m[INFO]\e[0m Updating apt packages using apt-get update -y"
 sudo apt-get update -y
-echo "[TASK] apt-get update of package cache complete, installing..." 
+echo -e "\e[1;33m[TASK]\e[0m apt-get update of package cache complete, installing..." 
 sudo apt-get install make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
-echo "[INFO] Installation complete."
+echo -e "\e[1;32m[INFO]\e[0m Installation complete."
