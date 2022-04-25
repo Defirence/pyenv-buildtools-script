3a32dd28a9c53f49aeaae07ab2dbdf4a66754e0f
Defirence <33593621+Defirence@users.noreply.github.com>
2022-04-25 12:16:18 +0200

    add -y flag to update task
    Signed-off-by: Defirence <33593621+Defirence@users.noreply.github.com>

--- install-python-buildenv.sh
+++ install-python-buildenv.sh
@@ -5,5 +5,5 @@ sudo apt-get update -y
 echo "[TASK] apt-get update of package cache complete, installing..." 
 sudo apt-get install make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
-libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
 echo "[INFO] Installation complete."
