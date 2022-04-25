014556ff07c7edb17b5be2856c9ef7e5f63ab561
Defirence <33593621+Defirence@users.noreply.github.com>
2022-04-25 12:55:16 +0200

    merge dev into main branch (#1)
    * initial commit for v0.1
    * add -y flag to update task
    * Add README
    * Added ANSI escape codes to show status for INFO etc...
    * add newline to end of file.
    
    Signed-off-by: Defirence <33593621+Defirence@users.noreply.github.com>

---
+++ install-python-buildenv.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+echo -e "\e[1;36m[INIT]\e[0m Installing python-buildenv tools for pyenv..."
+echo -e "\e[1;32m[INFO]\e[0m Updating apt packages using apt-get update -y"
+sudo apt-get update -y
+echo -e "\e[1;33m[TASK]\e[0m apt-get update of package cache complete, installing..." 
+sudo apt-get install make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev -y
+echo -e "\e[1;32m[INFO]\e[0m Installation complete."
